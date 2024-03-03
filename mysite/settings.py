95ebb202d22851ea83558cb5f328ba6c381526cf
Paul Owuor <85417768+paulowuor@users.noreply.github.com>
2024-03-03 23:43:08 +0300

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
